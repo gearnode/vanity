ffa26ed9e1fb8c332daedc842eadb953b36f6d93
gearnode <bryan@frimin.fr>
2024-01-12 11:53:35 +0400

    Add command line interface with configuration

---
+++ cmd/vanity/main.go
@@ -0,0 +1,114 @@
+// Copyright (c) 2023 Bryan Frimin <bryan@frimin.fr>.
+//
+// Permission to use, copy, modify, and/or distribute this software
+// for any purpose with or without fee is hereby granted, provided
+// that the above copyright notice and this permission notice appear
+// in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+// WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+// AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+// CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+// OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+// NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+// CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+package main // import "go.gearno.de/vanity/cmd/vanity"
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path"
+
+	"sigs.k8s.io/yaml"
+)
+
+const (
+	version = "0.0.1"
+)
+
+type (
+	Cfg struct {
+		DomainName string      `json:"domain-name"`
+		Imports    []ImportCfg `json:"imports"`
+	}
+
+	ImportCfg struct {
+		VCS          string `json:"vcs"`
+		RepoRoot     string `json:"repo-root"`
+		ImportPrefix string `json:"import-prefix"`
+	}
+)
+
+func fail(format string, args ...interface{}) {
+	fmt.Printf("error: "+format+"\n", args...)
+	os.Exit(1)
+}
+
+func loadCfgFile(filename string) (*Cfg, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open file: %w", err)
+	}
+
+	y, err := io.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read file: %w", err)
+	}
+
+	j, err := yaml.YAMLToJSON(y)
+	if err != nil {
+		return nil, fmt.Errorf("cannot convert yaml to json: %w", err)
+	}
+
+	cfg := Cfg{}
+	err = json.Unmarshal(j, &cfg)
+	if err != nil {
+		return nil, fmt.Errorf("cannot json unmarshal: %w", err)
+	}
+
+	return &cfg, nil
+}
+
+func main() {
+	cfgPath := flag.String("cfg", "", `the path of the configuration file (default "./vanity.yaml")`)
+	outputPath := flag.String("output", "", `the path where generated file will be written (default "./dist")`)
+	showHelp := flag.Bool("help", false, "shows this help message")
+	showVersion := flag.Bool("version", false, "prints the vanity cli version")
+
+	flag.Parse()
+
+	if *showHelp {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	if *showVersion {
+		fmt.Printf("vanity cli version %s\n", version)
+		os.Exit(0)
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		fail("cannot get current working directory: %v", err)
+	}
+
+	if *cfgPath == "" {
+		*cfgPath = path.Join(cwd, "vanity.yaml")
+	}
+
+	if *outputPath == "" {
+		*outputPath = path.Join(cwd, "dist")
+	}
+
+	cfg, err := loadCfgFile(*cfgPath)
+	if err != nil {
+		fail("cannot load configuration: %v", err)
+	}
+
+	fmt.Printf("%#+v\n", cfg)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go.gearno.de/vanity
 
 go 1.21.4
+
+require sigs.k8s.io/yaml v1.4.0
